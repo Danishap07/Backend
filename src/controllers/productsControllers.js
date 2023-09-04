482364a5dbf1dc74ea6f4fc0168e710a5cc7ba0d
danishap07 <danishp.1999@gmail.com>
2023-09-04 23:09:48 +0530

    handlederror

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -1,35 +1,35 @@
 import products from "../models/products";
 
 const createProduct = async (req, res) => {
-    const { product_name, actual_price, discounted_price, stock_quantity, category_id, sub_category_id, color, product_description, size } = req.body;
-    const { images } = req.files
+    const { product_name, actual_price, discounted_price, stock_quantity, category_id, sub_category_id, color, product_description, size, images } = req.body;
+    // const { images } = req.files
     // var isValid = true;
     if(!product_name) {
-       return res.status(400).json({ error: "Bad Request", message: "Product name is required." })
+       return res.status(400).json({status: false, error: "Bad Request", message: "Product name is required." })
     }
     else if(!actual_price) {
-        return res.status(400).json({ error: "Bad Request", message: "Actual Price is required." })
+        return res.status(400).json({status: false, error: "Bad Request", message: "Actual Price is required." })
      }
     else if(!discounted_price) {
-        return res.status(400).json({ error: "Bad Request", message: "Discounted Price is required." })
+        return res.status(400).json({status: false, error: "Bad Request", message: "Discounted Price is required." })
      }
     else if(!stock_quantity) {
-        return res.status(400).json({ error: "Bad Request", message: "Stock Quantity is required." })
+        return res.status(400).json({status: false, error: "Bad Request", message: "Stock Quantity is required." })
      }
     else if(!category_id) {
-        return res.status(400).json({ error: "Bad Request", message: "Category ID is required." })
+        return res.status(400).json({status: false, error: "Bad Request", message: "Category ID is required." })
      }
     else if(!sub_category_id) {
-        return res.status(400).json({ error: "Bad Request", message: "subCategory ID is required." })
+        return res.status(400).json({status: false, error: "Bad Request", message: "subCategory ID is required." })
      }
     else if(!color) {
-        return res.status(400).json({ error: "Bad Request", message: "Color of product is required." })
+        return res.status(400).json({status: false, error: "Bad Request", message: "Color of product is required." })
      }
     else if(!product_description) {
-        return res.status(400).json({ error: "Bad Request", message: "Product_description is required." })
+        return res.status(400).json({status: false, error: "Bad Request", message: "Product_description is required." })
      }
     else if(!size) {
-        return res.status(400).json({ error: "Bad Request", message: "Size is required." })
+        return res.status(400).json({status: false, error: "Bad Request", message: "Size is required." })
      }
     try{
      const result = await products.create({
@@ -49,10 +49,10 @@ const createProduct = async (req, res) => {
         "size": size
      })
      console.log(result);
-     return res.status(200).json({ message: `New Product ${product_name} created successfully.` })
+     return res.status(200).json({ status: true, message: `New Product ${product_name} created successfully.` })
     } 
     catch (error) {
-        return res.status(500).json({ message: error.message})
+        return res.status(500).json({status: false, message: error.message})
     }
 }
 
